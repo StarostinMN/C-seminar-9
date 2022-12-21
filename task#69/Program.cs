d5fccf5c386c4e72bdb955da2fd9872652ec298c
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-21 23:43:33 +0300

    add task#69

---
+++ task#69/Program.cs
@@ -0,0 +1,20 @@
+﻿/* Решение в группах задач:
+Задача 69: Напишите программу, которая на вход принимает два числа A и B, и возводит число А в целую степень B с помощью рекурсии.
+A = 3; B = 5 -> 243 (3⁵)
+A = 2; B = 3 -> 8 */
+
+Console.Clear();
+
+Console.Write("Введите возводимое в степень число: ");
+int A = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите показатель степени: ");
+int B = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"{A} ^ {B} = {ComputePow(A, B)}");
+
+int ComputePow(int a, int b)
+{
+    if (b == 0) return 1;
+    return a * ComputePow(a, b - 1);
+}
