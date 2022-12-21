e32c9fa6e31dbc72519d55eac58c46c391a09930
Mikhail Starostin <starostin.mihail@mail.ru>
2022-12-21 23:28:00 +0300

    add task#67

---
+++ task#67/Program.cs
@@ -0,0 +1,17 @@
+﻿/* Решение в группах задач:
+Задача 67: Напишите программу, которая будет принимать на вход число и возвращать сумму его цифр с помощью рекурсии.
+453 -> 12
+45 -> 9 */
+
+Console.Clear();
+
+Console.Write("Введите число: ");
+int Number = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine($"Сумма цифр в числе {Number} равна {ComputeSumOfDigit(Number)}");
+
+int ComputeSumOfDigit(int number)
+{
+    if (number / 10 == 0) return number % 10;
+    return number % 10 + ComputeSumOfDigit(number/10);
+}
